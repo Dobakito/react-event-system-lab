2ccef40efc5e1fa6a49e13f2db9dce852ca864f8
michael dobak <mdobak44@icloud.com>
2021-08-17 15:06:32 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+
+  handleOnBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  handleOnFocus = () => {
+    console.log('Good!');
+  }
+
+  render() {
+    return (
+      <button
+        onFocus={this.handleOnFocus}
+        onBlur={this.handleOnBlur}
+      ></button>
+    );
+  }
+
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+handleKeyPress = () => {
+  console.log('Entering password...');
+}
+
+  render() {
+    return (
+      <input
+      onKeyUp={this.handleKeyPress}
+      type="password"/>
+    );
+  }
+
+}
+
+export default Keypad;
